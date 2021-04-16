1610830e6ea736f50e51a7bbec7fe3cba9a98cc4
Jesús Alexis Ramírez Navor <jesus.ramirezn@alumno.buap.mx>
2021-04-15 22:19:22 -0500

    Checks arreglados

--- Carpooling/js/registro.js
+++ Carpooling/js/registro.js
@@ -12,13 +12,6 @@ $('#conductorDiv').hide();
 */
 $(function() {
 
-  // sets only one of the checkboxes avoiding checking more than one
-  $('.check-btn').click(function(e) {
-    
-    $('.check-btn').not(this).prop('checked', false); 
-
-  });  
-
   // When the driver checkbox is selected, two things can happen    
   $('#conductorCheck').on('click', function() {
 
@@ -28,12 +21,6 @@ $(function() {
       else
         $('#conductorDiv').hide();  // If it is unfocussed, it is hidden  
  
-  });
-  
-  $('#pasajero').click(function() {  
-
-      $('#conductorDiv').hide();  // Conductor driver is also hidden if passenger checkbox is selected        
-
   });
  
 });
