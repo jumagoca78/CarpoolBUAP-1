1a4b469fdf922ccfbe1efd2265d0d970e5844fe6
MarioAl21 <79184420+MarioAl21@users.noreply.github.com>
2021-04-18 04:40:36 -0500

    Update principal.php

--- Carpooling/Views/principal.php
+++ Carpooling/Views/principal.php
@@ -5,17 +5,18 @@ session_start();
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    
     <meta charset="utf-8"/>
     <title>Carpool BUAP</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- CDN CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
-
+    <!-- OWN CSS -->
     <link href="css/principal.css" rel="stylesheet">
+    
 </head>
 <body>
 
@@ -110,6 +111,11 @@ session_start();
     </div>
 </div>
 
+<!-- CDN SCRIPTS --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<!-- OWN SCRIPTS -->
 <script src="js/popup.js"></script>
 <script src="js/validate-email.js"></script>
 <script src="js/sweetalert.js"></script>
