7d0b209da32e4ea467c5bbf4eb6681ed5a2b5980
Hugo Chiquito <alex.400@live.com.mx>
2021-04-25 13:53:17 -0500

    Vista Tipo de Usuario

---
+++ Carpooling/Views/seleccionarusuario.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8"/>
+    <title>Carpool BUAP</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+
+    <link href="../css/registro.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- Navigation bar -->
+<nav class="navbar navbar-dark bg-primary">
+    <div class="container">
+        <a class="navbar-brand" href="../index.php">Carpool BUAP</a>
+    </div>
+</nav>
+
+<div class="container-lg text-align-center">
+  <div class="row">
+    <div class="mx-auto mt-5 m-3 p-5">
+      <button class="btn btn-primary btn-lg btn-block mb-5" type="button">Conductor</button>
+      <button class="btn btn-primary btn-lg btn-block" type="button">Pasajero</button>
+    </div>
+  </div>    
+</div>
+
+
+
+</body>
+</html>
+
