5f0edfb9668b273cb958b5e7ceeaa499e5ff5e55
Jesús Alexis Ramírez Navor <jesus.ramirezn@alumno.buap.mx>
2021-05-01 17:29:56 -0500

    Vista medallas

--- Carpooling/Views/medals.php
+++ Carpooling/Views/medals.php
@@ -10,7 +10,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
-    <link rel="stylesheet" href="../css/profile.css">
+    <link rel="stylesheet" href="../css/medals.css">
 </head>
 <body>
 
@@ -20,8 +20,8 @@
             <a class="fas fa-user-alt fa-3x" style="padding-right: 5rem" href="profile.php"></a>
             <a class="fas fa-medal fa-3x" href="medals.php"></a>
             <br>
-            <u style="padding-right: 4.2rem">Mi Perfil</u>
-            <a >Medallas</a>
+            <a style="padding-right: 4.2rem">Mi Perfil</a>
+            <u >Medallas</u>
         </div>
         <div class="text-left card-box">
             <div class="member-card pt-2 pb-2">
@@ -31,41 +31,32 @@
                 <div class="">
                     <h3>María Limón García</h3>
                     <p class="text-muted">Calificación <span>: </span><span><a class="fas fa-star"></a><a
-                                    class="fas fa-star"></a><a class="fas fa-star"></a><a
-                                    class="fas fa-star"></a></span></p>
+                                class="fas fa-star"></a><a class="fas fa-star"></a><a
+                                class="fas fa-star"></a></span></p>
                 </div>
-                <div class="mt-4">
-                    <h5>Acerca de mi: </h5>
-                    <p>Este es un ejemplo de los 250 caracteres que puedes incluir en tu bio, esto para evitar
-                        sobrecargar de información un apartado que debería ser meramente informativo. Como puedes
-                        observar la cantidad de texto que puedes incluir es más que suficiente.</p>
+                <br>
+                <div class="d-flex">
+                    <img src="../img/trofeo.png" class="img-medals"><br>
+                    <span>
+                        <h5>Conductor Del Mes</h5>
+                        <p>Fuiste el conductor mejor calificado en la plataforma del mes de Octubre 2021.</p>
+                    </span>
                 </div>
-                <div class="mt-4">
-                    <h5>Formación: </h5>
-                    <p>Ingeniería en Tecnlogías de la Información</p>
+                <div class="d-flex">
+                    <img src="../img/medalla1.png" class="img-medals"><br>
+                    <span>
+                        <h5>Puntualidad Primero</h5>
+                        <p>Fuiste el conductor mejor calificado en puntualidad en la plataforma por una semana.</p>
+                    </span>
                 </div>
-                <div class="mt-4">
-                    <h5>Unidad Academica: </h5>
-                    <p>Facultad de Ciencias de la Computación</p>
+                <div class="d-flex">
+                    <img src="../img/medalla2.png" class="img-medals"><br>
+                    <span>
+                        <h5>Segunda Liebre</h5>
+                        <p>Fuiste el segundo conductor mejor calificado en puntualidad en la plataforma por una semana.</p>
+                    </span>
                 </div>
-                <div class="mt-4">
-                    <h5>Correo: </h5>
-                    <p>maria.limongar@alumno.buap.mx</p>
-                </div>
-                <div class="mt-4">
-                    <h5>Dirección: </h5>
-                    <p>Coyoacan 57 Lomas de San Miguel Puebla, Puebla</p>
-                </div>
-                <div class="mt-4">
-                    <h5>Telefono: </h5>
-                    <p>2229172063</p>
-                </div>
-                <div class="text-center">
-                    <a id="btn-edit" class="btn btn-primary mt-3 btn-rounded" href="profile-edit.php">
-                        Editar
-                    </a>
-                    <br><br><br>
-                </div>
+                <br><br><br>
             </div>
         </div>
 </section>
