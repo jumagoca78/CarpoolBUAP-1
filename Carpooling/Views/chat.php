fe85e4d9c116655b9403fb83075aa532d42dae75
Hugo Chiquito <alex.400@live.com.mx>
2021-05-02 18:50:04 -0500

    ¡Mensajes Update!
    ¨Se agregó la vista de mensajes.

---
+++ Carpooling/Views/chat.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>CarPool</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link rel="stylesheet" href="../css/chat.css">
+</head>
+<body>
+    <header class="p-3">
+        <div class="container text-center">
+            <h3>Mensajes del grupo</h3>
+        </div>
+    </header>
+    <main>
+        <div class="container mb-3">
+            <div class="row">
+                <div class="thumb-lg member-thumb text-left ml-3 mb-3">
+                    <img src="../img/userejemplo.jpeg"
+                         class="rounded-circle img-thumbnail img"
+                         alt="profile-image">
+                </div>
+                <div class="mensaje-autor pl-2">
+                    <a class="autor">El Kebin</a>
+                    <div class="mensaje">
+                        ¿Les gusta Bad Bunny?
+                    </div>
+                </div>
+            </div>
+        </div>
+
+          <div class="container mb-3">
+            <div class="row">
+                <div class="thumb-lg member-thumb text-left ml-3 mb-3">
+                    <img src="../img/userejemplo.jpeg"
+                         class="rounded-circle img-thumbnail img"
+                         alt="profile-image">
+                </div>
+                <div class="mensaje-autor pl-2">
+                    <a class="autor">Maria</a>
+                    <div class="mensaje">
+                       No me desagrada en particular...
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container mb-3">
+            <div class="row">
+                <div class="thumb-lg member-thumb text-left ml-3 mb-3">
+                    <img src="../img/userejemplo.jpeg"
+                         class="rounded-circle img-thumbnail img"
+                         alt="profile-image">
+                </div>
+                <div class="mensaje-autor pl-2">
+                    <a class="autor">El Kebin</a>
+                    <div class="mensaje">
+                        ¿Puedes poner Bad Bunny cuando pases por mi?
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container mb-3">
+            <div class="row">
+                <div class="thumb-lg member-thumb text-left ml-3 mb-3">
+                    <img src="../img/userejemplo.jpeg"
+                         class="rounded-circle img-thumbnail img"
+                         alt="profile-image">
+                </div>
+                <div class="mensaje-autor pl-2">
+                    <a class="autor">Maria</a>
+                    <div class="mensaje">
+                        Claro, no hay problema!
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container mb-3">
+            <div class="row">
+                <div class="thumb-lg member-thumb text-left ml-3 mb-3">
+                    <img src="../img/userejemplo.jpeg"
+                         class="rounded-circle img-thumbnail img"
+                         alt="profile-image">
+                </div>
+                <div class="mensaje-autor pl-2">
+                    <a class="autor">Alexis</a>
+                    <div class="mensaje">
+                        ¿Alguien sabe como se cancela un viaje?
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container mb-3">
+            <div class="row">
+                <div class="thumb-lg member-thumb text-left ml-3 mb-3">
+                    <img src="../img/userejemplo.jpeg"
+                         class="rounded-circle img-thumbnail img"
+                         alt="profile-image">
+                </div>
+                <div class="mensaje-autor pl-2">
+                    <a class="autor">Hugo</a>
+                    <div class="mensaje">
+                        JAJAJA
+                    </div>
+                </div>
+            </div>
+        </div>    
+        <br><br>    
+    </main>
+    <footer>
+    <div class="input-group">
+        <button class="btn btn-outline-secondary btn-sm" type="button" aria-expanded="false"><img src="../img/emoji-button.svg" alt="x" />Emoji</button>
+        <input type="text" class="form-control form-control-lg"placeholder="Escribe un mensaje">
+        <button class="btn btn-outline-secondary btn-sm" type="button" aria-expanded="false"><img src="../img/enviar.svg" alt="x" />Enviar</button>
+    </div>        
+    </footer>
+</div>
+  
+</body>
+</html>
