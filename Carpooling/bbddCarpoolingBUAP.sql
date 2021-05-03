184058d40bf7f4f30ee44037bbd2db954837cf52
MarioAl21 <79184420+MarioAl21@users.noreply.github.com>
2021-05-03 10:58:09 -0500

    BBDD final

---
+++ Carpooling/bbddCarpoolingBUAP.sql
@@ -0,0 +1,127 @@
+-- MySQL dump 10.16  Distrib 10.1.38-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: carpoolingBUAP
+-- ------------------------------------------------------
+-- Server version	10.1.38-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `conductor`
+--
+
+DROP TABLE IF EXISTS `conductor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `conductor` (
+  `email` varchar(45) NOT NULL,
+  `id_conductor` int(11) NOT NULL AUTO_INCREMENT,
+  `licencia` text NOT NULL,
+  `seguro` text NOT NULL,
+  `tarjeta_circulacion` text NOT NULL,
+  `direccion_partida` varchar(50) DEFAULT NULL,
+  `direccion_destino` varchar(50) DEFAULT NULL,
+  `registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_conductor`),
+  KEY `email` (`email`),
+  CONSTRAINT `conductor_ibfk_1` FOREIGN KEY (`email`) REFERENCES `usuario` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `conductor`
+--
+
+LOCK TABLES `conductor` WRITE;
+/*!40000 ALTER TABLE `conductor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `conductor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `nombre` varchar(75) NOT NULL,
+  `apellido_paterno` varchar(75) NOT NULL,
+  `apellido_materno` varchar(75) NOT NULL,
+  `edad` int(11) NOT NULL,
+  `sexo` varchar(6) NOT NULL,
+  `matricula` int(9) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `tipo` char(1) NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `direccion` varchar(55) NOT NULL,
+  `perfil` text,
+  `formacion` text,
+  `unidad_academica` text,
+  `foto` text NOT NULL,
+  `tam_foto` bigint(20) DEFAULT NULL,
+  `password` varchar(21) NOT NULL,
+  `registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehiculo`
+--
+
+DROP TABLE IF EXISTS `vehiculo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vehiculo` (
+  `placa` varchar(9) NOT NULL,
+  `modelo` varchar(30) NOT NULL,
+  `marca` varchar(30) NOT NULL,
+  `color` varchar(20) NOT NULL,
+  `capacidad` int(11) NOT NULL,
+  `antiguedad` int(11) NOT NULL,
+  `id_conductor` int(11) NOT NULL,
+  `registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`placa`),
+  KEY `id_conductor` (`id_conductor`),
+  CONSTRAINT `vehiculo_ibfk_1` FOREIGN KEY (`id_conductor`) REFERENCES `conductor` (`id_conductor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehiculo`
+--
+
+LOCK TABLES `vehiculo` WRITE;
+/*!40000 ALTER TABLE `vehiculo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehiculo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-03 10:55:18
