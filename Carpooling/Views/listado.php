2667020a500fc8a66f036e3404734a8ed301203c
MarioAl21 <79184420+MarioAl21@users.noreply.github.com>
2021-05-03 10:33:35 -0500

    Views/listado.php: Consultas de la bbdd
    Es esta vista dónde tenemos 
    acceso a prácticamente toda la info de la
    bbdd a excepción de cierto datos sensibles como
    contraseñas. Por lo que, puede ser vista por cualquiera
    aunque se recomendaría que fuera algo exclusivo para el 
    admin. de la bbdd ;)

--- Carpooling/Views/listado.php
+++ Carpooling/Views/listado.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="es" xml:lang="es">
 <head>
 	<title>Listado de Imagnes</title>
@@ -34,8 +34,9 @@
 												<th>Matrícula</th>
 												<th>Email</th>
 												<th>Tipo</th>
-												<th>registro</th>
-												<th>tam_foto</th>
+												<th>Telefono</th>
+												<th>Direccion</th>
+												<th>Registro</th>
 											        <th></th>
 											</tr>
 										</thead>
@@ -78,17 +79,14 @@
 									<table id="tablaConductores" class="table table-striped table-hover table-bordered" width="100%">
 										<thead>
 											<tr>
-												<th>Nombre</th>
-												<th>Apellido paterno</th>
-												<th>Apellido materno</th>
-												<th>edad</th>
-												<th>sexo</th>
-												<th>Matrícula</th>
+												<th>Indentificador</th>
 												<th>Email</th>
-												<th>Tipo</th>
 												<th>registro</th>
-												<th>tam_foto</th>
-											        <th></th>
+												<th>Partida</th>
+												<th>Destino</th>
+												<th></th>
+												<th></th>
+												<th></th>	
 											</tr>
 										</thead>
 									</table>
@@ -100,8 +98,85 @@
 			</div>
 		</div>
 	</div>
-
-         	
+    
+	<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" id="modalfoto_licen">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title">VISUALIZAR FOTO</h4>
+				</div>
+				<div class="modal-body">
+					<div id="verfotomodal_licen"></div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-danger btn-sm" data-dismiss="modal"> Cerrar</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" id="modalfoto_seguro">
+		<div class="modal-dialog" role="document">	
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title">VISUALIZAR FOTO</h4>
+				</div>
+				<div class="modal-body">
+					<div id="verfotomodal_seguro"></div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-danger btn-sm" data-dismiss="modal"> Cerrar</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" id="modalfoto_tc">
+		<div class="modal-dialog" role="document">	
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title">VISUALIZAR FOTO</h4>
+				</div>
+				<div class="modal-body">
+					<div id="verfotomodal_tc"></div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-danger btn-sm" data-dismiss="modal"> Cerrar</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+    <div class="container">
+		<div class="row-fluid">
+			<h1 class="text-center mt-3">Vehiculos registrados</h1>
+			<div class="col-lg-12">
+				<div class="row">
+					<div class="card-body pad table-responsive">
+						<div class="row">
+							<div class="col-12">
+								<div class="table-responsive">
+									<table id="tablaVehiculos" class="table table-striped table-hover table-bordered" width="100%">
+										<thead>
+											<tr>
+												<th>Dueño</th>
+												<th>Placa</th>
+												<th>Modelo</th>
+												<th>Marca</th>
+												<th>Color</th>
+												<th>Capacidad</th>
+												<th>Antigüedad</th>
+											</tr>
+										</thead>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>     	
 
 
 	<script src="../libs/js/jquery.min.js"></script>
